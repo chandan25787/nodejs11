e1b3ad94b58fc650fa26a71154a48a162b56ebba
chandan25787 <40294371+chandan25787@users.noreply.github.com>
2018-06-15 16:29:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-'use strict';
-
-var fs = require('fs');
-var path = require('path');
-
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
-
-  callback(null, result);
-};
+console.log("hello ji");
